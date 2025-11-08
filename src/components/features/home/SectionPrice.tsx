e8fd7f3cda4a6bb61e4cedc815211f31a18abcca
hendriktarigann01 <hendriktarigan25@gmail.com>
2025-11-08 17:11:24 +0700

    fix: add baseUrl in tsconfig.json

--- src/components/features/home/SectionPrice.tsx
+++ src/components/features/home/SectionPrice.tsx
@@ -7,7 +7,7 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuTrigger,
+  DropdownMenuTrigger, 
 } from "@/components/ui/dropdown-menu";
 import { ChevronDown } from "lucide-react";
 import { useTranslations } from "next-intl";
